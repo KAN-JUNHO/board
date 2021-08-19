04f636a3f07e1a77f9cb0e21e6bfd7bab02108fe
junho <junho6667@gmail.com>
2021-08-19 15:21:48 +0900

    52021/08/19

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -7,7 +7,9 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.board.domain.BoardVO;
 import com.board.service.BoardService;
@@ -27,10 +29,17 @@ public class BoardController {
 		
 		model.addAttribute("list",list);
 	}
-	
 	//게시물 작성
 	@GetMapping("/write")
 	public void getWrite() throws Exception{
 		
+	}
+	
+	// 게시물 작성
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String posttWirte(BoardVO vo) throws Exception {
+	  service.write(vo);
+	  
+	  return "redirect:/board/list";
 	}
 }
